9e8aafc96c4dace7ea209392d18e9029dcbc27ad
Dane4ka5 <g05052011@yandex.ru>
2026-09-11 15:27:29 +0300

    Добавлена dev-система: вход и панель разработчика

--- server.js
+++ server.js
@@ -7,7 +7,22 @@ const PORT = process.env.PORT || 3000;
 // Отдаём статику из папки public
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Перенаправляем все запросы на index.html (работает в любой версии Express)
+// Главная страница
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Страница входа для разработчиков
+app.get('/dev-login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'dev-login.html'));
+});
+
+// Панель разработчика
+app.get('/dev-panel', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'dev-panel.html'));
+});
+
+// Всё остальное → на главную
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
